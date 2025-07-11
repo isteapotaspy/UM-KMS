dc2a3d1a947a99ab9ebf0ea9acbaf6d1084d71da
Ravin Chavez <ravinpaul.chavez@gmail.com>
2025-07-11 21:52:04 +0800

    final app layout design

--- src/um_lms_javafx/ui/user/student/dashboard/DashboardController.java
+++ src/um_lms_javafx/ui/user/student/dashboard/DashboardController.java
@@ -6,7 +6,9 @@ package um_lms_javafx.ui.user.student.dashboard;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 /**
  * FXML Controller class
@@ -15,12 +17,13 @@ import javafx.fxml.Initializable;
  */
 public class DashboardController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private Label announcementText;
+            
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        announcementText.setText("[1] Break rooms in the library is currently not  available due to maintenance.\n[2] Further, borrowing is temporarily on hold due to update in our library management system.");
+        announcementText.setWrapText(true);
     }    
     
 }
